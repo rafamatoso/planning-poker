ddd3d63425a598a62b2011b8aa075cd494b57495
Rafael Matoso <rafael.gustavo.gomes@accenture.com>
2025-01-28 17:27:42 -0300

    feat: add auth middleware

---
+++ backend/src/middleware/auth/index.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../../config/dotenv";
+import { AuthRequest } from "./interfaces";
+
+export const authenticateToken = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
