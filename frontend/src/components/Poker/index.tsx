f6118fc7215c44ce789f6cc1604eb75caf8d91b8
Rafael Matoso <rafael.gustavo.gomes@accenture.com>
2025-01-29 19:04:21 -0300

    feat: fe: update PokerPage component

--- frontend/src/components/Poker/index.tsx
+++ frontend/src/components/Poker/index.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import socket from "../../socket";
+import { IVote } from "./interfaces";
+
+const VOTE_NUMBERS = ["1", "2", "3", "5", "8", "13", "21", "34"];
 
 const Poker = () => {
-  const [votes, setVotes] = useState<{ [key: string]: string }>({});
+  const [votes, setVotes] = useState<IVote>({});
 
   useEffect(() => {
     socket.on("updateVotes", (data) => {
@@ -15,33 +18,33 @@ const Poker = () => {
   }, []);
 
   const sendVote = (value: string) => {
-    const newVotes = { ...votes, [`${socket.id}`]: value };
-    socket.emit("vote", newVotes);
+    socket.emit("vote", value);
   };
 
   return (
-    <div>
-      <h2>Planning Poker</h2>
-      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
-        {["1", "2", "3", "5", "8", "13"].map((num) => (
-          <button key={num} onClick={() => sendVote(num)}>
+    <div className="w-dvw flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h2 className="text-3xl font-bold text-gray-800 mb-6">Planning Poker</h2>
+
+      <div className="flex gap-4">
+        {VOTE_NUMBERS.map((num) => (
+          <button
+            key={num}
+            onClick={() => sendVote(num)}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition"
+          >
             {num}
           </button>
         ))}
       </div>
 
-      <h3
-        style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}
-      >
-        Votos:
-      </h3>
-      {/* <ul> */}
-      {Object.entries(votes).map(([id, vote]) => (
-        <li key={id}>
-          {id}: {vote}
-        </li>
-      ))}
-      {/* </ul> */}
+      <h3 className="text-xl font-semibold text-gray-700 mt-8">Votos:</h3>
+      <ul className="bg-white shadow-md rounded-lg p-4 mt-4 w-80">
+        {Object.entries(votes).map(([id, { username, vote }]) => (
+          <li key={id} className="text-gray-800">
+            <strong>{username}</strong>: {vote}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ frontend/src/page/Poker/index.tsx
@@ -0,0 +1,9 @@
+import Poker from "../../components/Poker";
+
+export default function PokerPage() {
+  return (
+    <div className="flex flex-col  items-center justify-center min-h-screen">
+      <Poker />
+    </div>
+  );
+}
