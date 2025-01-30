cf14455be610051fc62d58f12f44cea64dd73f77
Rafael Matoso <rafael.gustavo.gomes@accenture.com>
2025-01-30 07:51:20 -0300

    feat: fe: add lib react-tostify and fonts Lato

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Certifique-se de incluir a pasta correta
+  ],
   theme: {
     extend: {},
   },
