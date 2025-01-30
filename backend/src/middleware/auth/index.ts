3491c986806686d24cdfac7111c963a24e41f93c
Rafael Matoso <rafael.gustavo.gomes@accenture.com>
2025-01-30 07:57:34 -0300

    feat: be: update authenticateToken middleware

--- backend/src/middleware/auth/index.ts
+++ backend/src/middleware/auth/index.ts
@@ -1,24 +1,43 @@
 import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../../config/dotenv";
+import { getUserFromToken } from "../../utils/auth";
 import { AuthRequest } from "./interfaces";
 
-export const authenticateToken = (
+export const authenticateToken = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  try {
+    if (!token) {
+      res.status(401).json({ message: "Access denied" });
+    } else {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+      next();
+    }
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
+
+export const authenticateUser = (
   req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.header("Authorization")?.split(" ")[1];
+  const token = req.headers?.authorization?.split(" ")[1]; // "Bearer token"
 
-  if (!token) {
-    return res.status(401).json({ message: "Access denied" });
+  const user = getUserFromToken(token || "");
+
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(403).json({ message: "Invalid token" });
-  }
+  req.user = user; // Armazena os dados do usuário na request
+
+  next();
 };
